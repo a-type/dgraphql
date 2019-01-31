0715c24d401db42084c209838d97436994ef6a2a
Grant Forrest <gaforres@gmail.com>
2019-01-30 23:05:47 -0500

    cleanup

--- src/DGraphQL.ts
+++ src/DGraphQL.ts
@@ -1,48 +1,25 @@
 import {
   GraphQLResolveInfo,
-  SelectionSetNode,
   OperationDefinitionNode,
-  SelectionNode,
   GraphQLSchema,
   GraphQLType,
   GraphQLObjectType,
   GraphQLInterfaceType,
   GraphQLFieldMap,
-  GraphQLScalarType,
 } from 'graphql';
 import { DgraphClient } from 'dgraph-js';
-import {
-  DGraphQLResolverTree,
-  ResolverArgs,
-  DGraphQLOptions,
-  DGraphAugmentedSelectionSetNode,
-  DGraphQLQueryResolver,
-  DGraphAugmentedOperationDefinitionNode,
-  DGraphAugmentedFieldNode,
-  QueryDetailsFunc,
-  QueryResolver,
-} from './types';
+import { ResolverArgs, QueryDetailsFunc, QueryResolver } from './types';
 import { transformDocument } from 'graphql-ast-tools';
-import {
-  extractSelectionFieldName,
-  extractSelectionArguments,
-} from './extractors';
 import uuid from 'uuid';
-
-const DEFAULT_OPTIONS: DGraphQLOptions = {
-  defaultQueryResolver: (args: ResolverArgs, _ctx: any) => ({}),
-};
 
 export default class DGraphQL {
   private client: DgraphClient;
-  private options: DGraphQLOptions;
   private queryDetailsFuncsById: { [id: string]: QueryDetailsFunc } = {};
   private queryDetailsFuncsByPath: { [path: string]: QueryDetailsFunc } = {};
   private hasReadSchema = false;
 
-  constructor(client: DgraphClient, options: DGraphQLOptions) {
+  constructor(client: DgraphClient) {
     this.client = client;
-    this.options = options;
   }
 
   createQueryResolver = (queryDetailsFunc: QueryDetailsFunc): QueryResolver => {
@@ -102,46 +79,10 @@ export default class DGraphQL {
 
       const operation = simplifiedInfoDocument
         .definitions[0] as OperationDefinitionNode;
-
-      const augmentedAST = this.mergeFieldASTWithResolvedInfo(
-        operation,
-        context,
-      );
     };
 
     resolver.id = id;
 
     return resolver;
   };
-
-  private mergeFieldASTWithResolvedInfo = (
-    operation: OperationDefinitionNode,
-    context: any,
-  ): DGraphAugmentedOperationDefinitionNode => ({
-    ...operation,
-    selectionSet: this.recursivelyMergeSelectionASTWithResolvedInfo(
-      'foo',
-      operation.selectionSet,
-      context,
-    ),
-  });
-
-  private recursivelyMergeSelectionASTWithResolvedInfo = (
-    parentType: string,
-    selection: SelectionSetNode,
-    context: any,
-  ): DGraphAugmentedSelectionSetNode<DGraphAugmentedFieldNode> => ({
-    ...selection,
-    selections: selection.selections.map<DGraphAugmentedFieldNode>(
-      (selectionNode: SelectionNode): DGraphAugmentedFieldNode => {
-        const fieldName = extractSelectionFieldName(selectionNode);
-        const args = extractSelectionArguments(selectionNode);
-
-        return {
-          ...selectionNode,
-          dgraph: this.getQueryResolver(parentType, fieldName)(args, context),
-        } as DGraphAugmentedFieldNode;
-      },
-    ),
-  });
 }
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,4 @@
-import {
-  GraphQLResolveInfo,
-  SelectionNode,
-  FieldNode,
-  SelectionSetNode,
-  OperationDefinitionNode,
-} from 'graphql';
+import { GraphQLResolveInfo } from 'graphql';
 
 export type GenericArgs = {
   [argName: string]: any;
@@ -12,51 +6,8 @@ export type GenericArgs = {
 
 export type ResolverArgs = GenericArgs;
 
-export type DGraphQLQueryFragmentDefinition = {
-  func?: string;
-  filter?: string;
-  arguments?: GenericArgs;
-  alias?: string;
-};
-
-export type DGraphQLQueryResolver = (
-  args: ResolverArgs,
-  context: any,
-) => DGraphQLQueryFragmentDefinition;
-
-export type DGraphQLQueryTypeResolver = {
-  [fieldName: string]: DGraphQLQueryResolver;
-};
-
-export type DGraphQLResolverTree = {
-  Query: DGraphQLQueryTypeResolver;
-};
-
-export type DGraphQLOptions = {
-  defaultQueryResolver: DGraphQLQueryResolver;
-};
-
-export type DGraphAugmentedSelectionSetNode<
-  SelectionNodeType
-> = SelectionSetNode & {
-  selections: ReadonlyArray<SelectionNodeType>;
-};
-
-export type DGraphAugmentedFieldNode = FieldNode & {
-  readonly dgraph: DGraphQLQueryFragmentDefinition;
-  readonly selectionSet: DGraphAugmentedSelectionSetNode<
-    DGraphAugmentedFieldNode
-  >;
-};
-
-export type DGraphAugmentedOperationDefinitionNode = OperationDefinitionNode & {
-  readonly selectionSet: DGraphAugmentedSelectionSetNode<
-    DGraphAugmentedFieldNode
-  >;
-};
-
 /**
- * Begin: types for query building constructs
+ * Types for query building constructs
  */
 export type UID = string;
 
@@ -87,3 +38,7 @@ export type QueryResolver = {
   (parent: any, args: ResolverArgs, ctx: any, info: GraphQLResolveInfo): any;
   id: string;
 };
+
+/**
+ * Types that define the AST from which we generate the DGraph query
+ */
